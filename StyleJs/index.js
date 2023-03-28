88ba522ca45c994ca08bf476f0c744ac8ec1afea
zohirul22 <shahin57islam@gmail.com>
2023-03-29 00:35:31 +0600

    home page responsive

---
+++ StyleJs/index.js
@@ -0,0 +1,8 @@
+
+    let btnM = document.getElementById('btnM');
+    let navLinks = document.querySelector('.nav-links');
+
+    btnM.onclick = () =>{
+        btnM.classList.toggle('fa-times');
+        navLinks.classList.toggle('active')
+    }
